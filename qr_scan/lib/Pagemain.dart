b46ac6e3839002def7b6497d879420b1ba45532c
ParwaanVirk <2019uee0111@iitjammu.ac.in>
2020-04-23 23:04:22 +0530

    "Made the login frontend"

---
+++ qr_scan/lib/Pagemain.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:intl/intl.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'main.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  String result = "Hey There !";
+  String imageUrl = "";
+  bool isImage = false;
+  bool isApproved = false;
+  List<String> emailPhonePurposeApproved;
+
+  doAccept(approved) async {
+    if (!approved) {
+      DateTime now = DateTime.now();
+      DocumentSnapshot docSnapdocRef = await Firestore.instance
+          .collection("users")
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .get();
+      DocumentReference docRef = Firestore.instance
+          .collection('users')
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .collection("inout_register")
+          .document();
+
+      DocumentReference stateRef = Firestore.instance
+          .collection('users')
+          .document(emailPhonePurposeApproved[0].substring(0, 11));
+      DocumentReference stateRef2 = Firestore.instance
+          .collection('users_outside')
+          .document(emailPhonePurposeApproved[0].substring(0, 11));
+
+      Firestore.instance.runTransaction((Transaction tx) async {
+        await tx.update(stateRef, {"state": "2"});
+        // await tx.set(stateRef2, {"docRef": docRef.documentID});
+        await tx.update(docRef, {
+          'approved': true,
+          // 'in_datetime': DateFormat('yyyy-MM-dd-HH:mm:ss').format(now),
+          'out_datetime': DateFormat('yyyy-MM-dd-HH:mm:ss').format(now),
+          // 'phone': emailPhonePurposeApproved[1],
+          // 'purpose': emailPhonePurposeApproved[2],
+        });
+      }).then((rst) {
+        print(rst);
+        Fluttertoast.showToast(msg: 'Entry Successful');
+        setState(() {
+          isImage = false;
+          imageUrl = "";
+          result = "Hey There !";
+        });
+
+        return true;
+      }).catchError((error) {
+        print("$error");
+        Fluttertoast.showToast(msg: 'Error: $error');
+        return false;
+      });
+    } else {
+      DateTime now = DateTime.now();
+      DocumentReference outsideDocRef = await Firestore.instance
+          .collection("users")
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .get()
+          .then((onValue) {
+        DocumentReference docRef = Firestore.instance
+            .collection('users')
+            .document(emailPhonePurposeApproved[0].substring(0, 11))
+            .collection("inout_register")
+            .document(onValue.data["docRef"]);
+        DocumentReference stateRef = Firestore.instance
+            .collection('users')
+            .document(emailPhonePurposeApproved[0].substring(0, 11));
+        DocumentReference stateRef2 = Firestore.instance
+            .collection('users_outside')
+            .document(emailPhonePurposeApproved[0].substring(0, 11));
+
+        Firestore.instance.runTransaction((Transaction tx) async {
+          await tx.update(stateRef, {"state": "0"});
+          await tx.delete(stateRef2);
+          await tx.update(docRef, {
+            'in_datetime': DateFormat('yyyy-MM-dd-HH:mm:ss').format(now),
+          });
+        }).then((rst) {
+          print(rst);
+          Fluttertoast.showToast(msg: 'Entry Successful');
+          setState(() {
+            isImage = false;
+            imageUrl = "";
+            result = "Hey There !";
+          });
+
+          return true;
+        }).catchError((error) {
+          print("$error");
+          Fluttertoast.showToast(msg: 'Error: $error');
+          return false;
+        });
+      });
+    }
+  }
+
+  doReject(approved) async {
+    if (!approved) {
+      DocumentSnapshot docsnap = await Firestore.instance
+          .collection('users')
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .get();
+      DocumentReference canceling = Firestore.instance
+          .collection("users")
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .collection("inout_register")
+          .document(docsnap.data['docRef']);
+      DocumentReference docRefdocRef = Firestore.instance
+          .collection("users")
+          .document(emailPhonePurposeApproved[0].substring(0, 11));
+      DocumentSnapshot docSnapdocRef = await Firestore.instance
+          .collection("users")
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .get();
+      Map x = docSnapdocRef.data;
+      x.remove("docRef");
+
+      // docSnapdocRef.data.remove("docRef");
+      x["state"] = "0";
+      // print(x);
+      Firestore.instance.runTransaction((Transaction tx) async {
+        await tx.delete(canceling);
+        await tx.set(docRefdocRef, x);
+      }).then((rst) {
+        setState(() {
+          isImage = false;
+          imageUrl = "";
+          result = "Hey There !";
+        });
+      }).catchError((error) {
+        // _state = MyState.Inside;
+        // _isloading = false;
+        // notifyListeners();
+        print("$error");
+        Fluttertoast.showToast(msg: 'Error: $error');
+        return false;
+      });
+    } else {
+      setState(() {
+        isImage = false;
+        imageUrl = "";
+        result = "Hey There !";
+      });
+    }
+  }
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      emailPhonePurposeApproved = qrResult.split("_");
+
+      DocumentSnapshot userImgSnap = await Firestore.instance
+          .collection("users")
+          .document(emailPhonePurposeApproved[0].substring(0, 11))
+          .get();
+      // print(userImgSnap.data);
+      setState(() {
+        try {
+          imageUrl = userImgSnap.data['imgURL'];
+          isImage = true;
+          if (emailPhonePurposeApproved[3] == "1") {
+            isApproved = true;
+          } else {
+            isApproved = false;
+          }
+        } catch (e) {
+          print(e);
+          return false;
+        }
+        result = qrResult;
+        return true;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera Permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "you pressed the back button before scanning";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final GoogleSignIn _gSignIn = GoogleSignIn();
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              FontAwesomeIcons.signOutAlt,
+              size: 20.0,
+              color: Colors.white,
+            ),
+            onPressed: (){
+               _gSignIn.signOut();
+              print('Signed out');
+               Navigator.pop(context);
+             
+            },
+          ),
+        ]
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(height: 200, child: ExpenseList()),
+          isImage
+              ? Image(
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit.contain,
+                )
+              : Center(
+                  child: Text(
+                    result,
+                    style: new TextStyle(
+                        fontSize: 30.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+          isImage
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () {
+                        doAccept(isApproved);
+                      },
+                      child: Text("ACCEPT"),
+                      color: Colors.green,
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        doReject(isApproved);
+                      },
+                      color: Colors.red,
+                      child: Text("REJECT"),
+                    )
+                  ],
+                )
+              : Text(""),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+    );
+  }
+}
+
+class ExpenseList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection("users_outside").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return new Text("Loading");
+          return ListView(
+            children: getExpenseItems(snapshot),
+            itemExtent: 40,
+          );
+        });
+  }
+
+  getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot) {
+    List<DocumentSnapshot> x = snapshot.data.documents;
+    x.removeWhere((item) => item.documentID == "null");
+    if (x.length != 0) {
+      return snapshot.data.documents
+          .map((doc) => ListTile(
+                title: new Text(doc.documentID),
+
+                // subtitle: new Text(doc.documentID)
+              ))
+          .toList();
+    }
+    else{
+      return [ListTile(title:Text("No one is outside"))];
+    }
+  }
+}
--- qr_scan/lib/main.dart
+++ qr_scan/lib/main.dart
@@ -1,298 +1,118 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:barcode_scan/barcode_scan.dart';
-import 'package:flutter/services.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'Pagemain.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ));
+void main() => runApp(MyApp());
 
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() {
-    return new HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  String result = "Hey There !";
-  String imageUrl = "";
-  bool isImage = false;
-  bool isApproved = false;
-  List<String> emailPhonePurposeApproved;
-
-  doAccept(approved) async {
-    if (!approved) {
-      DateTime now = DateTime.now();
-      DocumentSnapshot docSnapdocRef = await Firestore.instance
-          .collection("users")
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .get();
-      DocumentReference docRef = Firestore.instance
-          .collection('users')
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .collection("inout_register")
-          .document();
-
-      DocumentReference stateRef = Firestore.instance
-          .collection('users')
-          .document(emailPhonePurposeApproved[0].substring(0, 11));
-      DocumentReference stateRef2 = Firestore.instance
-          .collection('users_outside')
-          .document(emailPhonePurposeApproved[0].substring(0, 11));
-
-      Firestore.instance.runTransaction((Transaction tx) async {
-        await tx.update(stateRef, {"state": "2"});
-        // await tx.set(stateRef2, {"docRef": docRef.documentID});
-        await tx.update(docRef, {
-          'approved': true,
-          // 'in_datetime': DateFormat('yyyy-MM-dd-HH:mm:ss').format(now),
-          'out_datetime': DateFormat('yyyy-MM-dd-HH:mm:ss').format(now),
-          // 'phone': emailPhonePurposeApproved[1],
-          // 'purpose': emailPhonePurposeApproved[2],
-        });
-      }).then((rst) {
-        print(rst);
-        Fluttertoast.showToast(msg: 'Entry Successful');
-        setState(() {
-          isImage = false;
-          imageUrl = "";
-          result = "Hey There !";
-        });
-
-        return true;
-      }).catchError((error) {
-        print("$error");
-        Fluttertoast.showToast(msg: 'Error: $error');
-        return false;
-      });
-    } else {
-      DateTime now = DateTime.now();
-      DocumentReference outsideDocRef = await Firestore.instance
-          .collection("users")
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .get()
-          .then((onValue) {
-        DocumentReference docRef = Firestore.instance
-            .collection('users')
-            .document(emailPhonePurposeApproved[0].substring(0, 11))
-            .collection("inout_register")
-            .document(onValue.data["docRef"]);
-        DocumentReference stateRef = Firestore.instance
-            .collection('users')
-            .document(emailPhonePurposeApproved[0].substring(0, 11));
-        DocumentReference stateRef2 = Firestore.instance
-            .collection('users_outside')
-            .document(emailPhonePurposeApproved[0].substring(0, 11));
-
-        Firestore.instance.runTransaction((Transaction tx) async {
-          await tx.update(stateRef, {"state": "0"});
-          await tx.delete(stateRef2);
-          await tx.update(docRef, {
-            'in_datetime': DateFormat('yyyy-MM-dd-HH:mm:ss').format(now),
-          });
-        }).then((rst) {
-          print(rst);
-          Fluttertoast.showToast(msg: 'Entry Successful');
-          setState(() {
-            isImage = false;
-            imageUrl = "";
-            result = "Hey There !";
-          });
-
-          return true;
-        }).catchError((error) {
-          print("$error");
-          Fluttertoast.showToast(msg: 'Error: $error');
-          return false;
-        });
-      });
-    }
-  }
-
-  doReject(approved) async {
-    if (!approved) {
-      DocumentSnapshot docsnap = await Firestore.instance
-          .collection('users')
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .get();
-      DocumentReference canceling = Firestore.instance
-          .collection("users")
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .collection("inout_register")
-          .document(docsnap.data['docRef']);
-      DocumentReference docRefdocRef = Firestore.instance
-          .collection("users")
-          .document(emailPhonePurposeApproved[0].substring(0, 11));
-      DocumentSnapshot docSnapdocRef = await Firestore.instance
-          .collection("users")
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .get();
-      Map x = docSnapdocRef.data;
-      x.remove("docRef");
-
-      // docSnapdocRef.data.remove("docRef");
-      x["state"] = "0";
-      // print(x);
-      Firestore.instance.runTransaction((Transaction tx) async {
-        await tx.delete(canceling);
-        await tx.set(docRefdocRef, x);
-      }).then((rst) {
-        setState(() {
-          isImage = false;
-          imageUrl = "";
-          result = "Hey There !";
-        });
-      }).catchError((error) {
-        // _state = MyState.Inside;
-        // _isloading = false;
-        // notifyListeners();
-        print("$error");
-        Fluttertoast.showToast(msg: 'Error: $error');
-        return false;
-      });
-    } else {
-      setState(() {
-        isImage = false;
-        imageUrl = "";
-        result = "Hey There !";
-      });
-    }
-  }
-
-  Future _scanQR() async {
-    try {
-      String qrResult = await BarcodeScanner.scan();
-      emailPhonePurposeApproved = qrResult.split("_");
-
-      DocumentSnapshot userImgSnap = await Firestore.instance
-          .collection("users")
-          .document(emailPhonePurposeApproved[0].substring(0, 11))
-          .get();
-      // print(userImgSnap.data);
-      setState(() {
-        try {
-          imageUrl = userImgSnap.data['imgURL'];
-          isImage = true;
-          if (emailPhonePurposeApproved[3] == "1") {
-            isApproved = true;
-          } else {
-            isApproved = false;
-          }
-        } catch (e) {
-          print(e);
-          return false;
-        }
-        result = qrResult;
-        return true;
-      });
-    } on PlatformException catch (ex) {
-      if (ex.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() {
-          result = "Camera Permission was denied";
-        });
-      } else {
-        setState(() {
-          result = "Unknown Error $ex";
-        });
-      }
-    } on FormatException {
-      setState(() {
-        result = "you pressed the back button before scanning";
-      });
-    } catch (ex) {
-      setState(() {
-        result = "Unknown Error $ex";
-      });
-    }
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("QR Scanner"),
+    return MaterialApp(
+      title: 'Flutter Google Signin APP',   // this is the sign in with google icon 
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff9C58D2),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(height: 200, child: ExpenseList()),
-          isImage
-              ? Image(
-                  image: NetworkImage(imageUrl),
-                  fit: BoxFit.contain,
-                )
-              : Center(
-                  child: Text(
-                    result,
-                    style: new TextStyle(
-                        fontSize: 30.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-          isImage
-              ? Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    RaisedButton(
-                      onPressed: () {
-                        doAccept(isApproved);
-                      },
-                      child: Text("ACCEPT"),
-                      color: Colors.green,
-                    ),
-                    RaisedButton(
-                      onPressed: () {
-                        doReject(isApproved);
-                      },
-                      color: Colors.red,
-                      child: Text("REJECT"),
-                    )
-                  ],
-                )
-              : Text(""),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.camera_alt),
-        label: Text("Scan"),
-        onPressed: _scanQR,
-      ),
+      home: GoogleSignApp(),
     );
   }
 }
 
-class ExpenseList extends StatelessWidget {
+class GoogleSignApp extends StatefulWidget {  
+  @override
+  _GoogleSignAppState createState() => _GoogleSignAppState();
+}
+
+class _GoogleSignAppState extends State<GoogleSignApp> {
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googlSignIn = new GoogleSignIn();
+
+Future<FirebaseUser> _signIn(BuildContext context) async {
+  
+   Scaffold.of(context).showSnackBar(new SnackBar(
+          content: new Text('Sign in'),
+        ));
+
+    final GoogleSignInAccount googleUser = await _googlSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =await googleUser.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+  
+    Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (context) => new HomePage(),
+      ),
+    );
+    
+  }
+
+ 
   @override
   Widget build(BuildContext context) {
-    return new StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection("users_outside").snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return new Text("Loading");
-          return ListView(
-            children: getExpenseItems(snapshot),
-            itemExtent: 40,
-          );
-        });
-  }
+    return Scaffold(
+      body: Builder(
+        builder: (context) => Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+           Container(
+             width: MediaQuery.of(context).size.width,
+             height:  MediaQuery.of(context).size.height,
+                child: Image.network(
+                   'https://images.unsplash.com/photo-1518050947974-4be8c7469f0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                   ,fit: BoxFit.fill,
+                   color: Color.fromRGBO(255, 255, 255, 0.6),
+                  colorBlendMode: BlendMode.modulate
+                ),
+              ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height:10.0),
+             Container(
+                  width: 250.0,
+                    child: Align(
+                  alignment: Alignment.center,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    color: Color(0xffffffff),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                      Icon(FontAwesomeIcons.google,color: Color(0xffCE107C),),
+                      SizedBox(width:10.0),
+                      Text(
+                      'Sign in with Google',
+                      style: TextStyle(color: Colors.black,fontSize: 18.0),
+                    ),
+                    ],),
+                    onPressed: () => _signIn(context)
+                              .then((FirebaseUser user) => print(user))
+                              .catchError((e) => print(e)),
+                  ),
+                )
+                ),
 
-  getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot) {
-    List<DocumentSnapshot> x = snapshot.data.documents;
-    x.removeWhere((item) => item.documentID == "null");
-    if (x.length != 0) {
-      return snapshot.data.documents
-          .map((doc) => ListTile(
-                title: new Text(doc.documentID),
-
-                // subtitle: new Text(doc.documentID)
-              ))
-          .toList();
-    }
-    else{
-      return [ListTile(title:Text("No one is outside"))];
-    }
+                
+            ],
+          ),
+        ],
+      ),),
+    );
   }
 }
+
+
+
+
+
+
